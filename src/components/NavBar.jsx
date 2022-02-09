82359e56bf10a4786f3cae2a4853d036a4d5e05c
Poyraz <poyraztcc@gmail.com>
2022-02-09 10:05:47 -1000

    made newdirectory

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext.jsx';
 
 const NavBar = () => {
---
+++ src/components/studio/swe4/Editor.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Editor extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <h3>This is the Audio Editor</h3>
+    );
+  }
+}
+
+export default Editor;
