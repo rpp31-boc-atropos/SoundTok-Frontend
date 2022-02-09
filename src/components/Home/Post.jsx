63af515b354b7f2e664871fc9ee0e76a37dd62b6
xoxohorses <69382434+xoxohorses@users.noreply.github.com>
2022-02-08 23:24:36 -0800

    Refactored home after removing feed

--- src/components/Home/Post.jsx
+++ src/components/Home/Post.jsx
@@ -94,21 +94,3 @@ const Component = () => {
 };
 
 export default Component;
-
-/* data
-{
-  profilePicture: "https://i.pinimg.com/474x/a3/89/f5/a389f597020f361f7f6d9b79323598fc.jpg",
-  timePosted: "2004-10-19 10:23:54+02", // this would just be when the row gets created. data type called timestamp in pg
-  userName: "atrophos",
-  postLikes: 123, // STRETCH
-  postSaved: false, // STRETCH
-  postText: "I have been waiting to release this for so long. pls no hate thx. #meow",
-  hashTags: [
-    "meow"
-  ], // not sure if we need this but when a post is created with a hashtag, think we should send
-  the list of hashtags as its own object/array so it can be added to the hashtag table
-  projectAudioLink: "someaudiolink",
-  projectTitle: "group meow",
-  projectLength: "98" // this should be in seconds
-}
-*/
