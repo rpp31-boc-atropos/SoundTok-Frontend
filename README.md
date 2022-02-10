dd902179b14fe6aea67cd297edddfaf506ef0e80
Yu Zhang <rita890927@gmail.com>
2022-02-09 21:24:19 -0500

    Add audioPlayer Context

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 ### `npm install`
 
-### `npm start`
+### `npm run build`
 
-### `npm run test` then select watch usage
+### `npm run test`
---
+++ src/components/audioPlayer/AudioPlayer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './AudioPlayer.css';
+import './input.css';
+import Playlist from './playlist/Playilst';
+import Controls from './Controls';
+
+//Replace the playlist with feeds
+
+const AudioPlayer = () => {
+  return (
+    <div className="main">
+      <div className="right">
+        <Playlist />
+      </div>
+      <Controls />
+    </div>
+  );
+};
+
+export default AudioPlayer;
