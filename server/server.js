05962febadc39039e364266ff2d69b1b4c495642
Maggie Saldivia <maggiesaldivia@gmail.com>
2022-02-12 19:20:04 -0500

    add axios, sample fetch in home.jsx and get / route in the server

--- server/server.js
+++ server/server.js
@@ -3,46 +3,64 @@ const cors = require('cors');
 const jwt = require('express-jwt');
 const jwksRsa = require('jwks-rsa');
 // const { resolve } = require('path');
+const axios = require('axios');
 
-require('dotenv').config({
 
-});
+require('dotenv').config({});
 
 const app = express();
 
 app.use(express.static('public'));
 
-// const appOrigin = process.env.APP_ORIGIN;
-// const audience = process.env.AUTH0_AUDIENCE;
-// const issuer = process.env.AUTH0_ISSUER;
+const appOrigin = process.env.APP_ORIGIN;
+const audience = process.env.AUTH0_AUDIENCE;
+const issuer = process.env.AUTH0_ISSUER;
 
 
-// app.use(cors({ origin: appOrigin }));
+app.use(cors({ origin: appOrigin }));
 
-// const checkJwt = jwt({
-//   secret: jwksRsa.expressJwtSecret({
-//     cache: true,
-//     rateLimit: true,
-//     jwksRequestsPerMinute: 5,
-//     jwksUri: `${issuer}.well-known/jwks.json`,
-//   }),
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `${issuer}.well-known/jwks.json`,
+  }),
 
-//   audience: audience,
-//   issuer: issuer,
-//   algorithms: ['RS256'],
-// });
+  audience: audience,
+  issuer: issuer,
+  algorithms: ['RS256'],
+});
 
-// app.get("/api/public-message", (req, res) => {
-//   res.send({
-//     msg: "The API doesn't require an access token to share this message.",
-//   });
-// });
+app.get("/api/public-message", (req, res) => {
+  res.send({
+    msg: "The API doesn't require an access token to share this message.",
+  });
+});
 
-// app.get("/api/private-message", checkJwt, (req, res) => {
-//   res.send({
-//     msg: "The API successfully validated your access token.",
-//   });
-// });
+app.get("/api/private-message", checkJwt, (req, res) => {
+  res.send({
+    msg: "The API successfully validated your access token.",
+  });
+});
+
+//backend routes - replace localhost with deployed URL when ready
+
+
+app.get("/", async (req, res) => {
+  const optionGetPosts = {
+    method: 'GET',
+    url: `http://localhost:1234/`,
+  }
+
+  axios(optionGetPosts)
+    .then((result) => {
+      console.log(result.data)
+    .catch((error) => {
+      console.log(error)
+      res.send(error)})
+  });
+})
 
 
 app.listen(3000, () => {
