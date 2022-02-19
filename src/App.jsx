2f98b09ce64dc6f7787655b0ca057f7304ee2277
xoxohorses <69382434+xoxohorses@users.noreply.github.com>
2022-02-18 19:17:13 -0800

    Finish rebasing

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,6 @@
-<<<<<<< HEAD
-import "./App.css";
-import React from "react";
-import {
-  HashRouter,
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
-import NavBar from "./components/Nav/NavBar.jsx";
-import Home from "./pages/Home.jsx";
-import Profile from "./pages/Profile.jsx";
-import NotFound from "./pages/NotFound.jsx";
-import Studio from "./pages/Studio.jsx";
-// import { useAuth } from './contexts/AuthContext.jsx';
-import AudioPlayer from "./components/audioPlayer/AudioPlayer";
-import Auth0ProviderWithHistory from "./components/Authentication/Auth0.jsx";
-import { useAuth0 } from "@auth0/auth0-react";
-import Hashtag from "./pages/Hashtag.jsx";
-=======
 import './App.css';
 import * as React from 'react';
+import axios from 'axios';
 import { HashRouter, BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import NavBar from './components/Nav/NavBar.jsx';
 import Home from './pages/Home.jsx';
@@ -31,7 +11,6 @@ import AudioPlayer from './components/audioPlayer/AudioPlayer';
 import Auth0ProviderWithHistory from './components/Authentication/Auth0.jsx';
 import { useAuth0 } from '@auth0/auth0-react';
 import { UserInfoContext } from './contexts/UserContext.jsx';
->>>>>>> b6dfc69 (Mid implementation of global userInfo context)
 
 //add new route above NotFound Route
 
@@ -43,39 +22,36 @@ const App = () => {
   const { user, isAuthenticated } = useAuth0();
   const [ userInfo, setUserInfo ] = React.useState({});
 
-  console.log(isAuthenticated);
-
   // QUERY FOR USER PROPIC AND USERNAME HERE USING EMAIL
   React.useEffect(() => {
-    console.log({user, isAuthenticated});
+    /* USER OBJECT PROPERTIES (i.e. user.email)
+    email: "juliemyu@gmail.com"
+    email_verified: true
+    family_name: "Yu"
+    given_name: "Julie"
+    locale: "en"
+    name: "Julie Yu"
+    nickname: "juliemyu"
+    picture: "https://lh3.googleusercontent.com/a-/AOh14GiDG3BTanmO3ioJmG7tQrm5IC7lv1LhCtljtcnzXaI=s96-c"
+    sub: "google-oauth2|113407150088693247128"
+    updated_at: "2022-02-19T02:14:17.669Z"
+    */
 
+    if (isAuthenticated) {
+      const email = user.email;
+      // backend get request using email as a key. we want profile picture, bio, username
+    }
   }, [isAuthenticated]);
 
   const callApi = () => {
     axios.get('/public')
       .then(response => console.log(response.date))
-      .catch(error => console.log(error))
+      .catch(error => console.log(error));
   };
 
   return (
     <HashRouter>
       <Auth0ProviderWithHistory>
-<<<<<<< HEAD
-        <div className="App">
-          <NavBar />
-          <Routes>
-            {/* <Fragment>
-              <PlayerProvider> */}
-            <Route path="/" element={<Home />} />
-            {/* <Route path='/profile/*' element={isAuthenticated ? <Profile /> : <Navigate to='/' />} /> */}
-            <Route path="/profile/*" element={<Profile />} />
-            <Route path="/studio/*" element={<Studio />} />
-            <Route path="/hashtag" element={<Hashtag />} />
-            <Route path="/*" element={<NotFound />} />
-          </Routes>
-          <AudioPlayer />
-        </div>
-=======
         <UserInfoContext.Provider value={{userInfo, setUserInfo}}>
           <div className='App'>
             <NavBar />
@@ -89,7 +65,6 @@ const App = () => {
             <AudioPlayer />
           </div>
         </UserInfoContext.Provider>
->>>>>>> b6dfc69 (Mid implementation of global userInfo context)
       </Auth0ProviderWithHistory>
     </HashRouter>
   );
