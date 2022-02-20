7b225918e8f4a26551fd37ec1fdde3daeb0c4764
Yuanqi Wang <zoeywang.zw@gmail.com>
2022-02-19 22:04:26 -0600

    Modified test for NavBar

--- test/NavBar.test.js
+++ test/NavBar.test.js
@@ -3,11 +3,52 @@
  */
 
 import React from 'react';
-import '@testing-library/jest-dom/extend-expect';
-import { render, screen } from './test-utils';
+// import '@testing-library/jest-dom/extend-expect';
+// import { render, screen } from './test-utils';
+import { render, screen } from '@testing-library/react';
+import { useAuth0 } from '@auth0/auth0-react';
+import { mocked } from 'jest/utils';
+
 import NavBar from '../src/components/Nav/NavBar.jsx';
 
-test('Heading should exist', () => {
-  render(<NavBar />);
-  expect(screen.getByText('SoundTok')).toBeInTheDocument;
+const user = {
+  email: "johndoe@me.com",
+  email_verified: true,
+  sub: "google-oauth2|12345678901234",
+};
+
+
+jest.mock("@auth0/auth0-react");
+
+const mockedUseAuth0 = mocked(useAuth0, true);
+
+describe("NavBar Component Tests - Logged in", () => {
+  beforeEach(() => {
+    mockedUseAuth0.mockReturnValue({
+      isAuthenticated: true,
+      user,
+      logout: jest.fn(),
+      loginWithRedirect: jest.fn(),
+      getAccessTokenWithPopup: jest.fn(),
+      getAccessTokenSilently: jest.fn(),
+      getIdTokenClaims: jest.fn(),
+      loginWithPopup: jest.fn(),
+      isLoading: false,
+    });
+  });
+  test("Logout Button displays when logged in", () => {
+    render(
+      <NavBar />
+    );
+    const loginButton = screen.getByText(/log out/i);
+    expect(loginButton).toBeInTheDocument();
+  });
+
 });
+
+
+
+// test('Heading should exist', () => {
+//   render(<NavBar />);
+//   expect(screen.getByText('SoundTok')).toBeInTheDocument;
+// });
