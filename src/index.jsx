3c437f8b447a3d54a61e484831289a266cb2205b
xoxohorses <69382434+xoxohorses@users.noreply.github.com>
2022-02-21 16:59:45 -0800

    Global Post context

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
-import { PlayerProvider } from './contexts/player/playerContext';
 import { HashRouter } from 'react-router-dom';
+import { PostsProvider } from './contexts/PostsContext.jsx';
+import { PlayerProvider } from './contexts/player/playerContext';
 import Auth0ProviderWithHistory from './components/authentication/Auth0.jsx';
 
 ReactDOM.render(
   <HashRouter>
     <Auth0ProviderWithHistory>
-      <PlayerProvider>
-        <App />
-      </PlayerProvider>
+      <PostsProvider>
+        <PlayerProvider>
+          <App />
+        </PlayerProvider>
+      </PostsProvider>
     </Auth0ProviderWithHistory>
   </HashRouter>,
   document.getElementById('root')
